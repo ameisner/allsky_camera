20c26dd7dad71ba2c94fb78dd2b02e94aa356757
Aaron Meisner <ameisner@fas.harvard.edu>
2021-10-23 06:15:42 -0700

    conversion to (Alt, Az), though currently very slow due to astropy infrastructure used

--- py/allsky_camera/starcat.py
+++ py/allsky_camera/starcat.py
@@ -8,6 +8,9 @@ A class representing a bright star catalog.
 import astropy.io.fits as fits
 import allsky_camera.util as util # needed?
 import allsky_camera.io as io
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+import astropy.units as u
 
 class StarCat:
     """
@@ -32,12 +35,31 @@ class StarCat:
             mjd - float
                 MJD (scalar) at which to compute (alt, az) as viewed from KPNO.
 
+        Returns
+        -------
+            altaz - astropy.coordinates.sky_coordinate.SkyCoord
+                List of (alt, az) pairs in degrees. Same length as
+                the bright star catalog.
+
         Notes
         -----
             Assumes the observer is at KPNO. MJD assumed to be scalar for now.
 
         """
-        pass
+        obstime = Time(str(mjd), format='mjd')
+
+        location = EarthLocation(lat=31.9599*u.deg, lon=-111.5997*u.deg,
+                                 height=2100.0*u.m)
+
+        coords = SkyCoord(self.catalog['RA']*u.deg, self.catalog['DEC']*u.deg)
+
+        # this is slow ...
+        # pressure is needed to account for atmospheric refraction,
+        # to match default behavior of IDL eq2hor
+        altaz = coords.transform_to(AltAz(obstime=obstime, location=location,
+                                          pressure=1.013*u.bar))
+
+        return altaz
 
     def cat_with_altaz(self, mjd):
         """
