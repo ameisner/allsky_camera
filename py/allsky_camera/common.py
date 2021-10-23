2ea1b692994674f38aff7b46d1af6478e662d808
Aaron Meisner <ameisner@fas.harvard.edu>
2021-10-23 06:43:19 -0700

    separate out parameters specific to a particular camera

--- py/allsky_camera/common.py
+++ py/allsky_camera/common.py
@@ -25,23 +25,26 @@ def ac_params():
 
     # may want to include astrometric solution parameters here as well
 
-    par = {'nx': 1600,
-           'ny': 1200,
-           'bitpix': 8,
-           'meta_env_var': 'ALLSKY_CAMERA_META',
-           'raw_satur_val': 255,
-           'exptime_card': 'EXPOSURE',
-           'static_mask_filename': 'static_badpix_mask.fits.gz',
-           'off_region_xmin': 1450,
-           'off_region_xmax': 1600,
-           'off_region_ymin': 1075,
-           'off_region_ymax': 1200,
-           'jd_minus_mjd' : 2400000.5,
-           'bsc_filename' : 'bsc5.fits',
-           'bsc_filename_csv' : 'bsc5.csv',
-           'kpno_lon' : -111.5997,
-           'kpno_lat' : 31.9599,
-           'kpno_elev_meters' : 2100.0,
-           'pressure_bars' : 1.013}
+    # camera-specific parameters
+    par_camera = {'nx': 1600,
+                  'ny': 1200,
+                  'bitpix': 8,
+                  'raw_satur_val': 255,
+                  'exptime_card': 'EXPOSURE',
+                  'static_mask_filename': 'static_badpix_mask.fits.gz',
+                  'off_region_xmin': 1450,
+                  'off_region_xmax': 1600,
+                  'off_region_ymin': 1075,
+                  'off_region_ymax': 1200}
 
-    return par
+    # miscellaneous parameters not tied to a particular camera
+    par_misc = {'meta_env_var': 'ALLSKY_CAMERA_META',
+                'jd_minus_mjd' : 2400000.5,
+                'bsc_filename' : 'bsc5.fits',
+                'bsc_filename_csv' : 'bsc5.csv',
+                'kpno_lon' : -111.5997,
+                'kpno_lat' : 31.9599,
+                'kpno_elev_meters' : 2100.0,
+                'pressure_bars' : 1.013}
+
+    return dict(par_camera, **par_misc)
