fdc7f9c2a5f1745a6d21bb18d358127cc14af15b
Aaron Meisner <ameisner@fas.harvard.edu>
2021-11-13 15:41:51 -0700

    docstring

--- py/allsky_camera/analysis/medfilt_parallel.py
+++ py/allsky_camera/analysis/medfilt_parallel.py
@@ -6,6 +6,43 @@ from multiprocessing import Pool
 import multiprocessing
 
 def ind_split_image(im, nchunks, ksize=23, axis=1):
+    """
+
+    Parameters
+    ----------
+        im : numpy.ndarray
+            2D image.
+        nchunks : int
+            Number of chunks into which to split the image along axis.
+        ksize : int (optional)
+            Median filter kernel size that will be applied downstream. In practice
+            this dictates the amount of padding needed for each chunk, which is
+            roughly ksize/2 pixels along each edge. ksize=0 is meant to behave
+            such that the indices returned split the image into
+            a set of mutually exclusive chunks.
+        axis: int (optional)
+            Axis along which it is desired to segment the image.
+            Should be either 0 or 1, given that this function is intended
+            for 2D images. Default is 1 (x axis).
+
+    Returns
+    -------
+        starts : list
+            List of starting indices of each image chunk. Length will be nchunks.
+        ends : list
+            List of ending indices of each image chunk (Python convention).
+            Length will be nchunks.
+
+    Notes
+    -----
+        Add a check to make sure nchunks is not larger than the image dimension along the
+        axis of splitting? Other potential checks along these lines?
+    """
+
+    # nchunks=1 is a trivial case that shouldn't
+    # actually be requested, but the correct answer is returned
+    # in that case nevertheless
+    assert(nchunks >= 1)
 
     pad = np.ceil(ksize/2)
 
@@ -15,17 +52,22 @@ def ind_split_image(im, nchunks, ksize=23, axis=1):
 
     npix = sh[axis]
 
-    # + 1 is to be cautious/generous
-    half = (ksize // 2) + 1*(ksize != 0)
+    # + 1 is to be cautious/generous, may not be needed
+    # the ksize != 0 part is there to make it so that ksize=0
+    # results in mutually exclusive image chunks
+    khalf = (ksize // 2) + 1*(ksize != 0)
 
+    # starting indices of each chunk along the axis being segmented
     starts = []
+    # ending indices of each chunk along the axis being segmented (Python convention)
     ends = []
 
+    # size of each chunk in pixels
     size_approx = int(np.ceil(npix/float(nchunks)))
 
     for i in range(nchunks):
-        start = max(i*size_approx - half, 0)
-        end = min((i+1)*size_approx + half, npix)
+        start = max(i*size_approx - khalf, 0)
+        end = min((i+1)*size_approx + khalf, npix)
 
         if i == (nchunks-1):
             end = npix
@@ -37,13 +79,20 @@ def ind_split_image(im, nchunks, ksize=23, axis=1):
 
 def split_and_reassemble(im, nchunks=2, ksize=23, nmp=2):
 
+
+    # this is unfortunately hardcoded for now
+    # would be nice to generalize this to work for either
+    # axis, but would require some changes to how the
+    # indexing is treated below. Could also accomplish with
+    # a transposition hack...
+    axis = 1
     sh = im.shape
     assert(len(sh) == 2)
 
     assert(nmp > 1)
     assert(nmp <= multiprocessing.cpu_count())
 
-    starts_pad, ends_pad = ind_split_image(im, nchunks, ksize=ksize, axis=1)
+    starts_pad, ends_pad = ind_split_image(im, nchunks, ksize=ksize, axis=axis)
 
     # ksize=0 means splitting image into n chunks with
     # no padding/overlap of the chunks
